3f260ec2bbbc177c561637208ed928239ab8c433
Michael Light <mklight06@gmail.com>
2021-11-22 08:40:20 -0700

    Working query for productid, incorrect format

--- server/controllers/productId.js
+++ server/controllers/productId.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const models = require('../models');
+// const express = require('express');
+// const models = require('../models');
 
-const app = express();
+// const app = express();
 
-module.exports = {
-  get: (req, res) => {
-    // req.query and req.params, pass into invocation of getAll
-    // log req to see how it's structured
-    console.log('reqQuery', req.query);
-    models.productId.getOne(req.query)
-      .then((result) => {
-        console.log('result', result);
-        res.send(result);
-      });
-  },
-};
+// module.exports = {
+//   get: (req, res) => {
+//     // req.query and req.params, pass into invocation of getAll
+//     // log req to see how it's structured
+//     console.log('reqQuery', req.query);
+//     models.productId.getOne(req.query)
+//       .then((result) => {
+//         console.log('result', result);
+//         res.send(result);
+//       });
+//   },
+// };
--- server/models/products.js
+++ server/models/products.js
@@ -1,8 +1,9 @@
 // query for database
+const { features } = require('process');
 const pool = require('../../database');
 
 const getAll = async (params) => {
-  console.log(params);
+  // console.log(params);
   const client = await pool.connect();
   const { product_id } = params;
   const { count } = params || 5;
@@ -10,8 +11,44 @@ const getAll = async (params) => {
   try {
     // await client.query('begin');
     if (product_id) {
-      const idResult = await client.query('select * from products where id = $1', [product_id]);
-      console.log('idResult', idResult);
+      //   {
+      //     "id": 11,
+      //     "name": "Air Minis 250",
+      //     "slogan": "Full court support",
+      //     "description": "This optimized air cushion pocket reduces impact but keeps a perfect balance underfoot.",
+      //     "category": "Basketball Shoes",
+      //     "default_price": "0",
+      //     "features": [
+      //     {
+      //             "feature": "Sole",
+      //             "value": "Rubber"
+      //         },
+      //     {
+      //             "feature": "Material",
+      //             "value": "FullControlSkin"
+      //         },
+      //     // ...
+      //     ],
+      // }
+      const idResult = await client.query(`select json_build_object(
+        'id', p.id,
+        'name', p.name,
+        'slogan', p.slogan,
+        'description', p.description,
+        'category', p.category,
+        'default_price', p.default_price,
+        'features', features
+      )products from products p
+      left join (select
+        product_id,
+        json_agg(
+          json_build_object(
+            'feature', f.feature,
+            'value', f.value
+          )
+        ) features from features f
+        group by product_id
+        ) f on p.id = f.product_id where id = $1`, [product_id]);
       return idResult.rows;
     }
     const result = await client.query('select * from products limit $1', [count]);
